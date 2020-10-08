43a02f12008d7c7049296a808a389a6da1d6d0bb
psycholja <71594643+psycholja@users.noreply.github.com>
2020-10-08 22:55:00 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_Employee
+{
+    class Employee
+    {
+        public string name1;
+        public string name2;
+        public int exp;
+        public int qual;
+        public int bonus;
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {name1}\nФамилия: {name2}\nСтаж:{exp}\nКвалификация:{qual}");
+        }
+        public int GetBonusPercent()
+        {
+            {
+                if (exp < 1)
+                {
+                    bonus = 0;
+                }
+                if ((exp>=1)&&(exp<5))
+
+                {
+                    bonus = 5;
+                }
+                
+                if ((exp>=5)&&(qual<3))
+                {
+                    bonus=10;
+                }
+                if ((exp>5)&&(qual==5))
+                {
+                    bonus = 20;
+                }
+                return bonus;
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+    
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp_Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee();
+            myEmployee.exp=10;
+            myEmployee.qual=5;
+            myEmployee.name1="Том";
+            myEmployee.name2="Иванов";
+            
+           
+
+
+            Console.WriteLine($"Имя: {myEmployee.name1} \nФамилия: {myEmployee.name2} \nСтаж:{myEmployee.exp} (лет) \nКвалификация:{myEmployee.qual} (1-5)");
+            Console.WriteLine(value: $"Бонус: {myEmployee.GetBonusPercent()} %");
+
+            Console.ReadKey();
+        }
+    }
+}
